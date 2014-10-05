6b210cd004a25406d8d822fb0043506e8e8724ac
David Wen <dstwen@gmail.com>
2014-10-04 17:25:37 -0700

    create_record api method

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,5 +1,63 @@
 class ApisController < ApplicationController
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
+  before_filter :allow_cors
+
   def create_record
-    
+    respond_to do |format|
+      user = User.find_by_uuid(params[:user_params][:uuid])
+      unless user
+        user = User.create(user_params)
+      end
+      game = Game.create(game_params)
+      game.update_attributes(:user_id => user.id)
+
+      attempts = params[:attempts]
+
+      attempts.keys.each do |key|
+        Attempt.create(
+          :guess => attempts[key]["guess"],
+          :result => attempts[key]["result"],
+          :game_id => game.id
+        )
+      end
+      format.json { render :json => { :status => "Great success!" } }
+    end
+  end
+
+  private
+
+  def allow_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
+  def user_params
+    params.require(:user_params).permit(
+      :uuid,
+      :username,
+      :platform,
+      :model,
+      :created_at,
+      :updated_at,
+      :_destroy
+    )
+  end
+
+  def game_params
+    params.require(:game_params).permit(
+      :answer,
+      :result,
+      :time,
+      :digits,
+      :repeat,
+      :created_at,
+      :updated_at,
+      :_destroy
+    )
   end
 end
